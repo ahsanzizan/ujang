5881a1880b148739fc7d8adc63adbf821d3f970a
Ahsan <ahsanaz461@gmail.com>
2024-02-25 06:53:20 +0700

    fix: Actually implement the visit method

--- src/interpreter.py
+++ src/interpreter.py
@@ -75,11 +75,19 @@ class Interpreter:
         expr = self.expr()
         return PrintStatement(expr)
 
-    def visit_printstatement(self, node):
+    def visit(self, node):
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+    
+    def generic_visit(self, node):
+        raise Exception('No visit_{} method'.format(type(node).__name__))
+        
+    def visit_PrintStatement(self, node):
         result = self.visit(node.expr)
         print(result)
 
-    def visit_binop(self, node):
+    def visit_BinOp(self, node):
         if node.op.type == PLUS:
             return self.visit(node.left) + self.visit(node.right)
         elif node.op.type == MINUS:
@@ -89,7 +97,7 @@ class Interpreter:
         elif node.op.type == DIV:
             return self.visit(node.left) / self.visit(node.right)
 
-    def visit_num(self, node):
+    def visit_Num(self, node):
         return node.value
 
     def interpret(self):
