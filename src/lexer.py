97190d2f160f9d46e68362e28831c0debaba5f24
Ahsan <ahsanaz461@gmail.com>
2024-02-25 06:55:35 +0700

    feat: Skip whitespaces

--- src/lexer.py
+++ src/lexer.py
@@ -8,42 +8,54 @@ class Lexer:
     def error(self):
         raise Exception('Invalid character')
     
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.text):
+            self.current_char = self.text[self.pos]
+        else:
+            self.current_char = None
+    
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+    
     def get_next_token(self):
-        # If the position is out of the text content
-        if self.pos >= len(self.text):
-            return Token(EOF, None)
-        
-        current_char = self.text[self.pos]
-        
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
-            return token
-        
-        if current_char == '+':
-            token = Token(PLUS, current_char)
-            self.pos += 1
-            return token
-        
-        if current_char == '-':
-            token = Token(PLUS, current_char)
-            self.pos += 1
-            return token
-        
-        if current_char == '*':
-            token = Token(MUL, current_char)
-            self.pos += 1
-            return token
-        
-        if current_char == '/':
-            token = Token(DIV, current_char)
-            self.pos += 1
-            return token
+        # If the position is still in the text content
+        while self.current_char is not None:
+            # Skips whitespace
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        if current_char == 'p':
-            token = Token(PRINT, current_char)
-            self.pos += 1
-            return token
+            if self.current_char.isdigit():
+                return Token(INTEGER, int(self.current_char))
 
-        self.error()
+            if self.current_char == '+':
+                token = Token(PLUS, self.current_char)
+                self.advance()
+                return token
+
+            if self.current_char == '-':
+                token = Token(MINUS, self.current_char)
+                self.advance()
+                return token
+
+            if self.current_char == '*':
+                token = Token(MUL, self.current_char)
+                self.advance()
+                return token
+
+            if self.current_char == '/':
+                token = Token(DIV, self.current_char)
+                self.advance()
+                return token
+
+            if self.current_char == 'p':
+                token = Token(PRINT, self.current_char)
+                self.advance()
+                return token
+
+            self.error()
+
+        return Token(EOF, None)
         
